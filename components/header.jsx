94aca32e058b493f647d655a8b45b92593f81842
Aqib Jawad <aqibjawad123@gmail.com>
2024-08-09 17:04:57 +0500

    header responsive for every screeen

--- components/header.jsx
+++ components/header.jsx
@@ -1,28 +1,17 @@
 import React from "react";
-
 import styles from "../styles/header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div
-        className=""
-        style={{ display: "flex", justifyContent: "space-between" }}
-      >
-        <div style={{ flex: 1, marginRight: "10px" }}>
-          <div className={styles.userName}>Ghulam Bari</div>
-
-          <div className={styles.userEmail}>Ghulam Bari@gmail.com</div>
-        </div>
-
-        <div style={{ flex: 1, marginRight: "30rem" }}>
-          <div className={styles.companyName}>Ghulam Bari Rice Mill</div>
-        </div>
-
-        <div>
-          <div className={styles.logoutBtn}>Logout</div>
-        </div>
+      <div className={styles.nameCont}>
+        <div className={styles.userName}>Ghulam Bari</div>
+        <div className={styles.userEmail}>Ghulam Bari@gmail.com</div>
       </div>
+
+      <div className={styles.companyName}>Ghulam Bari Rice Mill</div>
+
+      <div className={styles.logoutBtn}>Logout</div>
     </div>
   );
 };
